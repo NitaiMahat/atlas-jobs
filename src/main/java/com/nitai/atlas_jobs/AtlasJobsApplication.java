8b309fa92930adb9ddd8a3db76604051c2d14d0c
NitaiMahat <mahatnitai@gmail.com>
2026-02-02 15:22:30 -0600

    Initial Spring Boot project

---
+++ src/main/java/com/nitai/atlas_jobs/AtlasJobsApplication.java
@@ -0,0 +1,13 @@
+package com.nitai.atlas_jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasJobsApplication.class, args);
+	}
+
+}
